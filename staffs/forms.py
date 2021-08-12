615d9e0fbfdd4310b3a022cbab11fcea5d299be9
lonestarhiro <hirotoka@hotmail.com>
2021-08-13 02:40:27 +0900

    0812

--- staffs/forms.py
+++ staffs/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from staffs.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+#パスワードを含まない
 class StaffForm(forms.ModelForm):
     class Meta:
         model = User
@@ -10,6 +11,7 @@ class StaffForm(forms.ModelForm):
         'kanri','jimu','caremane','servkan','kaigo','yougu','kango','kinou','seikatu','ishi',
         'riha','ope','ryouyou','jihakan','sidou','hoiku','jisou','driver','eiyou','tyouri','gengo','tyounou')
 
+#パスワードを含む
 class NewStaffForm(UserCreationForm):
     class Meta:
         model = User

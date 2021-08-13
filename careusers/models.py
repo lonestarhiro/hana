92be657001c921c36e353bfe64ddd19e4b871d94
lonestarhiro <hirotoka@hotmail.com>
2021-08-13 16:25:46 +0900

    0813

--- careusers/models.py
+++ careusers/models.py
@@ -4,8 +4,10 @@ class Careuser(models.Model):
 
     gender_choise ={(0,"男"),(1,"女")}
   
-    first_kana = models.CharField(verbose_name="めい",max_length=30)
+    last_name  = models.CharField(verbose_name="姓",max_length=30)
+    first_name = models.CharField(verbose_name="名",max_length=30)
     last_kana  = models.CharField(verbose_name="せい",max_length=30)
+    first_kana = models.CharField(verbose_name="めい",max_length=30)
     gender     = models.PositiveSmallIntegerField(verbose_name="性別",max_length=1,choices=gender_choise),
     birthday   = models.DateField(verbose_name="生年月日",blank=True)
     user_no    = models.PositiveSmallIntegerField(verbose_name="利用者番号",blank=True, null=True,unique=True)

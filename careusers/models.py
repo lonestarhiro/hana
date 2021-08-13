67367a9e64b149e17fc376427c4be875802aa82e
lonestarhiro <hirotoka@hotmail.com>
2021-08-13 20:55:35 +0900

    0813

--- careusers/models.py
+++ careusers/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class Careuser(models.Model):
 
-    gender_choise ={(0,"男"),(1,"女")}
+    gender_choice =((0,"男"),(1,"女"),)
   
     last_name  = models.CharField(verbose_name="姓",max_length=30)
     first_name = models.CharField(verbose_name="名",max_length=30)
     last_kana  = models.CharField(verbose_name="せい",max_length=30)
     first_kana = models.CharField(verbose_name="めい",max_length=30)
-    gender     = models.PositiveSmallIntegerField(verbose_name="性別",max_length=1,choices=gender_choise),
+    gender     = models.PositiveSmallIntegerField(verbose_name="性別",default=0,choices=gender_choice)
     birthday   = models.DateField(verbose_name="生年月日",blank=True)
     user_no    = models.PositiveSmallIntegerField(verbose_name="利用者番号",blank=True, null=True,unique=True)
     postcode   = models.CharField(verbose_name="郵便番号",max_length=7)
@@ -21,3 +21,6 @@ class Careuser(models.Model):
     startdate  = models.DateField(verbose_name="契約日",blank=True)
     biko       = models.TextField(verbose_name="備考",default="",blank=True)
     is_active  = models.BooleanField(verbose_name="利用中",default=True)
+
+    def __str__(self):
+        return f"{self.last_name} + " " + {self.first_name}" 

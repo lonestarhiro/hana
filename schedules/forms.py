3dbfb42bd19292587851aefc318f59bfcd4b8f77
lonestarhiro <hirotoka@hotmail.com>
2021-08-27 17:44:43 +0900

    0826

--- schedules/forms.py
+++ schedules/forms.py
@@ -1,7 +1,13 @@
 from .models import Schedule
 from django import forms
 
-class ScheduleForm(forms.ModelForm):
+class ScheduleCreateForm(forms.ModelForm):
+    class Meta:
+        model = Schedule
+        exclude =  ('end_date','kaigo_point','shogai_point','from_default','check_flg','comfirm_flg')
+
+
+class ScheduleEditForm(forms.ModelForm):
     class Meta:
         model = Schedule
         exclude =  ('end_date','kaigo_point','shogai_point','from_default','check_flg','comfirm_flg')

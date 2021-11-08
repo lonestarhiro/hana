6372752e6362e6330a1672ef8997f57ea79ba1c7
hirotoka <hirotoka@hotmail.com>
2021-11-08 23:02:26 +0900

    1108

--- hana/settings.py
+++ hana/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-8_22lhc@dy69fa_ywf=e#m5+0_i@r%uk$-z#z_k099+9do3q3z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+#後日ホストを明示すること
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

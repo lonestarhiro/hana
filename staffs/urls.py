d13d51346f7b9b3728df274d6182400fa5da0bf1
hirotoka <hirotoka@hotmail.com>
2021-11-11 20:46:40 +0900

    1111

--- staffs/urls.py
+++ staffs/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views
 from django.urls import path
 from .views import StaffListView,StaffEditView,StaffCreateView
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 
 
 app_name = "staffs"
@@ -17,7 +18,7 @@ urlpatterns = [
 
 
     #ログイン済みの場合のみ
-    path("password_change/",login_required(views.PasswordChangeView.as_view(template_name="staffs/password_change.html")),name="password_change"),
+    path("password_change/",login_required(views.PasswordChangeView.as_view(template_name="staffs/password_change.html",success_url = reverse_lazy('staffs:password_change_done'))),name="password_change"),
     path("password_change/done/",login_required(views.PasswordChangeDoneView.as_view(template_name="staffs/password_change_done.html")),name="password_change_done"),
     
     #以下はsuperuserのみアクセス可能(viewsにて制限)

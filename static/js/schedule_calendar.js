36e56c8f9c61311fc3723ffa92a31671cfaf27e2
hirotoka <hirotoka@hotmail.com>
2021-11-30 17:34:28 +0900

    1130

--- static/js/schedule_calendar.js
+++ static/js/schedule_calendar.js
@@ -8,10 +8,10 @@ $(function() {
     //全体一覧表示時アンカーへスクロール
     $(window).on("load", function(){
       if ($("#anchor").length>0){
-        var headerH = 200; //ヘッダーの高さ
-        var target = $("#anchor").eq(0);
-        var position = target.offset().top;
-        var sc_pos = position - headerH;
+        //var headerH = 200; //ヘッダーの高さ
+        //var target = $("#anchor").eq(0);
+        //var position = target.offset().top;
+        //var sc_pos = position - headerH;
         //scrollTo(0,sc_pos);
         //$('html, body').animate({scrollTop:sc_pos},100,'swing');
         document.getElementById('anchor').scrollIntoView();

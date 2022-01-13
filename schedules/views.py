21d8ca83a46af6ee9869cea1c9a6d05657e45e7d
hirotoka <hirotoka@hotmail.com>
2022-01-13 21:25:04 +0900

    0113

--- schedules/views.py
+++ schedules/views.py
@@ -234,20 +234,22 @@ class ReportUpdateView(UpdateView):
         #社員の場合
         else:
             self.object.locked=True
+
+        if self.object.deposit is None:
+            self.object.deposit = 0;
+        
+        if self.object.payment is None:
+            self.object.payment = 0;
         
         form.save()
         return super(ReportUpdateView,self).form_valid(form)
 
     def get_success_url(self):
-        year  = self.object.service_in_date.year
-        month = self.object.service_in_date.month
-        day   = self.object.service_in_date.day
-        return reverse_lazy('schedules:dailylist',kwargs={'year':year,'month':month,'day':day})
+        return reverse_lazy('schedules:report_detail',kwargs={'pk':self.object.pk})
 
 class ReportDetailView(DetailView):
     model = Report
     template_name = "schedules/report_detail.html"
-    context_object_name = "report"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -260,11 +262,221 @@ class ReportDetailView(DetailView):
             #ロックされているデータは除外
             schedule_data = get_object_or_404(Report.objects.select_related('schedule'),(Q(schedule__staff1=self.request.user)|Q(schedule__staff2=self.request.user)|Q(schedule__staff3=self.request.user)|Q(schedule__staff4=self.request.user)),locked=True,pk=int(pk))
 
-        context['schedule_data'] = schedule_data
-        context['otsuri'] = schedule_data.deposit - schedule_data.payment
-        
+        context['report'] = self.report_for_output(schedule_data)
         return context
 
+    def report_for_output(self,schedule_data):
+        
+        obj={}
+        obj['pk'] = schedule_data.schedule.pk #pk
+        obj['careuser'] = schedule_data.schedule.careuser #利用者名
+        #サービススタッフ
+        txt=""
+        if schedule_data.schedule.peoples is 1:
+            txt += str(schedule_data.schedule.staff1)
+        elif schedule_data.schedule.peoples is 2:
+            txt += str(schedule_data.schedule.staff1) + " " + str(schedule_data.schedule.staff2)
+        elif schedule_data.schedule.peoples is 3:
+            txt += str(schedule_data.schedule.staff1) + " " + str(schedule_data.schedule.staff2) + " " + str(schedule_data.schedule.staff3)
+        elif schedule_data.schedule.peoples is 4:
+            txt += str(schedule_data.schedule.staff1) + " " + str(schedule_data.schedule.staff2) + " " + str(schedule_data.schedule.staff3) + " " + str(schedule_data.schedule.staff4)
+        if schedule_data.schedule.tr_staff1:
+            txt += " [同行] " + str(schedule_data.schedule.tr_staff1)
+        if schedule_data.schedule.tr_staff2:
+            txt += " " + str(schedule_data.schedule.tr_staff2)
+        if schedule_data.schedule.tr_staff3:
+            txt += " " + str(schedule_data.schedule.tr_staff3)
+        if schedule_data.schedule.tr_staff4:
+            txt += " " + str(schedule_data.schedule.tr_staff4)  
+        obj['helper'] = txt
+        obj['date'] = schedule_data.schedule.start_date #予定日時
+        obj['service_in_date']  = schedule_data.service_in_date #サービス開始日時
+        obj['service_out_date'] = schedule_data.service_out_date #サービス終了日時
+        obj['service'] = schedule_data.schedule.service
+        obj['first'] = schedule_data.first #初回加算
+        obj['emergency'] = schedule_data.emergency #緊急加算
+        #事前チェック
+        obj['pre_check'] = None
+        if schedule_data.face_color or schedule_data.hakkan or schedule_data.body_temp or (schedule_data.blood_pre_h and schedule_data.blood_pre_l):
+            txt = ""
+            txt += "顔色:" + schedule_data.get_face_color_display() + "　" if schedule_data.face_color else ""
+            txt += "発汗:" + schedule_data.get_hakkan_display() + "　" if schedule_data.hakkan else ""
+            txt += "体温:" + str(schedule_data.body_temp) + "℃　" if schedule_data.body_temp else ""
+            txt += "血圧:" + str(schedule_data.blood_pre_h) + "/" + str(schedule_data.blood_pre_l) if schedule_data.blood_pre_h and schedule_data.blood_pre_l else ""
+            obj['pre_check'] = txt
+        #排泄
+        obj['excretion'] = None
+        if schedule_data.toilet or schedule_data.p_toilet or schedule_data.Diapers or schedule_data.Pads or schedule_data.linen or schedule_data.inbu \
+            or schedule_data.nyouki or schedule_data.urination_t  or schedule_data.urination_a or schedule_data.defecation_t or schedule_data.defecation_s: 
+            txt = ""
+            txt += "トイレ介助　"   if schedule_data.toilet   else ""
+            txt += "Pトイレ介助　"  if schedule_data.p_toilet else ""
+            txt += "おむつ交換　"   if schedule_data.Diapers  else ""
+            txt += "パッド交換　"   if schedule_data.Pads     else ""
+            txt += "リネン等処理　" if schedule_data.linen    else ""
+            txt += "陰部清潔　"     if schedule_data.inbu     else ""
+            txt += "尿器洗浄　"     if schedule_data.nyouki   else ""
+            txt += "排尿回数:" + str(schedule_data.urination_t) + "回　"  if schedule_data.urination_t else ""
+            txt += "排尿量:" + str(schedule_data.urination_a) + "cc　"    if schedule_data.urination_a else ""
+            txt += "排便回数:" + str(schedule_data.defecation_t) + "回　" if schedule_data.defecation_t else ""
+            txt += "排便状態:" + schedule_data.defecation_s if schedule_data.defecation_s else ""
+            obj['excretion'] = txt
+        #食事
+        obj['eating'] = None
+        if schedule_data.posture or schedule_data.eating or schedule_data.eat_a or schedule_data.drink_a:
+            txt = ""
+            txt += "姿勢の確保　" if schedule_data.posture else ""
+            txt += "摂食介助:" + schedule_data.get_eating_display() + "　" if schedule_data.eating  else ""
+            txt += "食事量:" + str(schedule_data.eat_a) + "%　"            if schedule_data.eat_a   else ""
+            txt += "水分補給:" + str(schedule_data.drink_a) + "cc　"       if schedule_data.drink_a else ""
+            obj['eating'] =txt
+        #清拭入浴
+        obj['bath'] = None
+        if schedule_data.bedbath or schedule_data.bath or schedule_data.wash_hair:
+            txt = ""
+            txt += "清拭:" + schedule_data.get_bedbath_display() + "　" if schedule_data.bedbath else ""
+            txt += "入浴:" + schedule_data.get_bath_display() + "　"    if schedule_data.bath    else ""
+            txt += "洗髪　" if schedule_data.wash_hair else ""
+            obj['bath'] =txt
+        #身体整容
+        obj['beauty'] = None
+        if schedule_data.wash_face or schedule_data.wash_mouse or schedule_data.change_cloth or schedule_data.makeup_nail or schedule_data.makeup_ear or schedule_data.makeup_beard or schedule_data.makeup_hair or schedule_data.makeup_face:
+            txt = ""
+            txt += "洗面　"         if schedule_data.wash_face    else ""
+            txt += "口腔ケア　"     if schedule_data.wash_mouse   else ""
+            txt += "更衣介助　"     if schedule_data.change_cloth else ""
+            txt += "整容（爪）　"   if schedule_data.makeup_nail  else ""
+            txt += "整容（耳）　"   if schedule_data.makeup_ear   else ""
+            txt += "整容（髭）　"   if schedule_data.makeup_beard else ""
+            txt += "整容（髪）　"   if schedule_data.makeup_hair  else ""
+            txt += "整容（化粧）　" if schedule_data.makeup_face  else ""
+            obj['beauty'] =txt
+        #移動
+        obj['moving'] = None
+        if schedule_data.change_pos or schedule_data.movetransfer or schedule_data.move or schedule_data.readytomove or schedule_data.readytocome or schedule_data.gotohospital or schedule_data.gotoshopping:
+            txt = ""
+            txt += "体位変換　"     if schedule_data.change_pos   else ""
+            txt += "移乗介助　"     if schedule_data.movetransfer else ""
+            txt += "移動介助　"     if schedule_data.move         else ""
+            txt += "外出準備介助　" if schedule_data.readytomove  else ""
+            txt += "帰宅受入介助　" if schedule_data.readytocome  else ""
+            txt += "通院介助　"     if schedule_data.gotohospital else ""
+            txt += "買物介助　"     if schedule_data.gotoshopping else ""
+            obj['moving'] =txt
+        #起床就寝
+        obj['sleeping'] = None
+        if schedule_data.wakeup or schedule_data.goingtobed:
+            txt = ""
+            txt += "起床介助　" if schedule_data.wakeup     else ""
+            txt += "就寝介助　" if schedule_data.goingtobed else ""
+            obj['sleeping'] =txt
+        #服薬
+        obj['medicine'] = None
+        if schedule_data.medicine or schedule_data.medicine_app or schedule_data.eye_drops:
+            txt = ""
+            txt += "服薬介助・確認　" if schedule_data.medicine     else ""
+            txt += "薬の塗布　"       if schedule_data.medicine_app else ""
+            txt += "点眼　"           if schedule_data.eye_drops    else ""
+            obj['medicine'] =txt
+        #その他
+        obj['other'] = None
+        if schedule_data.in_hospital or schedule_data.watch_over:
+            txt = ""
+            txt += "院内介助　" if schedule_data.in_hospital else ""
+            txt += "見守り　"   if schedule_data.watch_over  else ""
+            obj['other'] =txt
+        #自立支援
+        obj['independence'] = None
+        if schedule_data.jir_together or schedule_data.jir_memory or schedule_data.jir_call_out or schedule_data.jir_shopping or schedule_data.jir_motivate:
+            txt = ""
+            txt += "共に行う(内容):" + schedule_data.jir_together + "　" if schedule_data.jir_together else ""
+            txt += "記憶への働きかけ　"   if schedule_data.jir_memory   else ""
+            txt += "声かけと見守り　"     if schedule_data.jir_call_out else ""
+            txt += "買物援助　"           if schedule_data.jir_shopping else ""
+            txt += "意欲関心の引き出し　" if schedule_data.jir_motivate else ""
+            obj['independence'] =txt
+        #清掃
+        obj['cleaning'] = None
+        if schedule_data.cl_room or schedule_data.cl_toilet or schedule_data.cl_table or schedule_data.cl_kitchen or schedule_data.cl_bath or schedule_data.cl_p_toilet or schedule_data.cl_bedroom or schedule_data.cl_hall or schedule_data.cl_front or schedule_data.cl_trush:
+            txt = ""
+            txt += "居室　"     if schedule_data.cl_room     else ""
+            txt += "トイレ　"   if schedule_data.cl_toilet   else ""
+            txt += "卓上　"     if schedule_data.cl_table    else ""
+            txt += "台所　"     if schedule_data.cl_kitchen  else ""
+            txt += "浴室　"     if schedule_data.cl_bath     else ""
+            txt += "Pトイレ　"  if schedule_data.cl_p_toilet else ""
+            txt += "寝室　"     if schedule_data.cl_bedroom  else ""
+            txt += "廊下　"     if schedule_data.cl_hall     else ""
+            txt += "玄関　"     if schedule_data.cl_front    else ""
+            txt += "ゴミ出し　" if schedule_data.cl_trush    else ""
+            obj['cleaning'] =txt
+        #洗濯
+        obj['washing'] = None
+        if schedule_data.washing or schedule_data.wash_dry or schedule_data.wash_inbox or schedule_data.wash_iron:
+            txt = ""
+            txt += "洗濯　"           if schedule_data.washing    else ""
+            txt += "乾燥(物干し)　"   if schedule_data.wash_dry   else ""
+            txt += "取り入れ・収納　" if schedule_data.wash_inbox else ""
+            txt += "アイロン　"       if schedule_data.wash_iron  else ""
+            obj['washing'] =txt
+        #寝具
+        obj['bedding'] = None
+        if schedule_data.bed_change or schedule_data.bed_making or schedule_data.bed_dry:
+            txt = ""
+            txt += "シーツ・カバー交換　" if schedule_data.bed_change else ""
+            txt += "ベッドメイク　"       if schedule_data.bed_making else ""
+            txt += "布団干し　"           if schedule_data.bed_dry    else ""
+            obj['bedding'] =txt
+        #衣類
+        obj['clothes'] = None
+        if schedule_data.cloth_sort or schedule_data.cloth_repair:
+            txt = ""
+            txt += "衣類の整理　" if schedule_data.cloth_sort   else ""
+            txt += "被服の補修　" if schedule_data.cloth_repair else ""
+            obj['clothes'] =txt
+        #調理
+        obj['cooking'] = None
+        if schedule_data.cooking or schedule_data.cook_lower or schedule_data.cook_prepare or schedule_data.cook_menu:
+            txt = ""
+            txt += "調理　"     if schedule_data.cooking      else ""
+            txt += "下拵え　"   if schedule_data.cook_lower   else ""
+            txt += "配・下膳　" if schedule_data.cook_prepare else ""
+            txt += "献立:" + schedule_data.cook_menu + "　" if schedule_data.cook_menu  else ""
+            obj['cooking'] =txt
+        #買物等
+        obj['shopping'] = None
+        if schedule_data.daily_shop or schedule_data.Receive_mad or schedule_data.deposit or schedule_data.payment:
+            txt = ""
+            txt += "日常品等買物　"     if schedule_data.daily_shop      else ""
+            txt += "薬の受取り　"   if schedule_data.Receive_mad   else ""
+            if schedule_data.deposit or schedule_data.payment:
+                depo  = "{:,}".format(schedule_data.deposit)#3桁区切りにする
+                pay   = "{:,}".format(schedule_data.payment)#3桁区切りにする
+                oturi = "{:,}".format(schedule_data.deposit-schedule_data.payment)#3桁区切りにする
+                txt += "預り金:" + depo + "円－買物:" + pay + "円＝おつり:" + oturi +"円　"
+            obj['shopping'] =txt
+        #備考
+        obj['biko'] = schedule_data.biko
+        #退室確認
+        obj['after_check'] = None
+        if schedule_data.after_fire or schedule_data.after_elec or schedule_data.after_water or schedule_data.after_close:
+            txt = ""
+            txt += "火元　"   if schedule_data.after_fire  else ""
+            txt += "電気　"   if schedule_data.after_elec  else ""
+            txt += "水道　"   if schedule_data.after_water else ""
+            txt += "戸締り　" if schedule_data.after_close else ""
+            obj['after_check'] =txt
+
+        #身体・生活それぞれの登録があるかどうか
+        obj['is_physical_care'] = False
+        obj['is_life_support']  = False
+        if obj['excretion'] or obj['eating'] or obj['bath'] or obj['beauty'] or obj['moving'] \
+             or obj['sleeping'] or obj['medicine'] or obj['other'] or obj['independence']:
+            obj['is_physical_care'] = True
+        if obj['cleaning'] or obj['washing'] or obj['bedding'] or obj['clothes'] or obj['cooking'] or obj['shopping']:
+            obj['is_life_support'] = True
+
+        return obj
 
 #以下staffuserのみ表示（下のStaffUserRequiredMixinにて制限中）
 

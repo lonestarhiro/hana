4cdfb3a67af7d72f023f432a9665c9e9f27c76b0
hirotoka <hirotoka@hotmail.com>
2022-01-26 10:57:50 +0900

    0126

--- schedules/views.py
+++ schedules/views.py
@@ -1,7 +1,7 @@
 from .models import Schedule,Report,ShowUserEnddate
 from staffs.models import User
-from careusers.models import CareUser
-from django.db.models import Q,Max
+from careusers.models import CareUser,Service
+from django.db.models import Q,Max,Prefetch
 from django.http import HttpResponseRedirect,Http404
 from hana.mixins import StaffUserRequiredMixin,SuperUserRequiredMixin,MonthWithScheduleMixin
 from django.urls import reverse_lazy,reverse
@@ -199,52 +199,40 @@ class ReportUpdateView(UpdateView):
         #登録ヘルパーは自身が入っているスケジュール以外でロックされていないデータ以外表示しないようにする。
         if self.request.user.is_staff:
             #obj = Report.objects.select_related('schedule').get(pk=int(pk))
-            obj = get_object_or_404(Report.objects.select_related('schedule'),pk=int(pk))
+            obj = get_object_or_404(Report.objects.prefetch_related(Prefetch("schedule",queryset=Schedule.objects.select_related('service'),to_attr="sche")),pk=int(pk))
+            #print(list(vars(obj.sche.service)))
         else:
-            obj = get_object_or_404(Report.objects.select_related('schedule'),search_relate_staff_tr_query(self.request.user),careuser_comfirmed=False,pk=int(pk))
+            obj = get_object_or_404(Report.objects.prefetch_related(Prefetch("schedule",queryset=Schedule.objects.select_related('service'),to_attr="sche")),search_relate_staff_tr_query(self.request.user),careuser_comfirmed=False,pk=int(pk))
         return obj
-    
-    def get_form(self):
-        form = super().get_form(self.form_class)
-        #行先入力を必須のサービスの場合
-        if self.object.schedule.service.destination:
-            form.fields['destination'].required = True
-        #複合サービスの場合
-        if self.object.schedule.service.mix_items:
-            form.fields['in_time_main'].required = True
-            form.fields['in_time_sub'].required = True
-        form.fields['biko'].required = True
-        return form
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial={}
+        if not self.object.service_in_date  : initial['service_in_date']  = self.object.schedule.start_date
+        if not self.object.service_out_date : initial['service_out_date'] = self.object.schedule.end_date
+        if not self.object.in_time_main     : initial['in_time_main']     = self.object.schedule.service.in_time_main
+        if not self.object.in_time_sub      : initial['in_time_sub']      = self.object.schedule.service.in_time_sub
+        return initial
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        if self.object.service_in_date is None:
-            #formに初期値をセット
-            form = ReportForm(initial={
-                'service_in_date' : self.object.schedule.start_date,
-                'service_out_date': self.object.schedule.end_date,
-                'in_time_main'    : self.object.schedule.service.in_time_main,
-                'in_time_sub'     : self.object.schedule.service.in_time_sub,
-            })
-            context['form'] = form
-
         helpers=""
         if self.object.schedule.peoples == 1:
             helpers += str(self.object.schedule.staff1)
         elif self.object.schedule.peoples == 2:
-            helpers += str(self.object.schedule.staff1) + "　" + str(self.object.schedule.staff2)
+            helpers += str(self.object.schedule.staff1) + "<br class=\"d-md-none\">" + str(self.object.schedule.staff2)
         elif self.object.schedule.peoples == 3:
-            helpers += str(self.object.schedule.staff1) + "　" + str(self.object.schedule.staff2) + "　" + str(self.object.schedule.staff3)
+            helpers += str(self.object.schedule.staff1) + "<br class=\"d-md-none\">" + str(self.object.schedule.staff2) + "<br class=\"d-md-none\">" + str(self.object.schedule.staff3)
         elif self.object.schedule.peoples == 4:
-            helpers += str(self.object.schedule.staff1) + "　" + str(self.object.schedule.staff2) + "　" + str(self.object.schedule.staff3) + "　" + str(self.object.schedule.staff4)
+            helpers += str(self.object.schedule.staff1) + "<br class=\"d-md-none\">" + str(self.object.schedule.staff2) + "<br class=\"d-md-none\">" + str(self.object.schedule.staff3) + "<br class=\"d-md-none\">" + str(self.object.schedule.staff4)
         if self.object.schedule.tr_staff1:
-            helpers += " 　[同行] " + str(self.object.schedule.tr_staff1)
+            helpers += " <br class=\"d-md-none\">[同行] " + str(self.object.schedule.tr_staff1)
         if self.object.schedule.tr_staff2:
-            helpers += "　" + str(self.object.schedule.tr_staff2)
+            helpers += "<br class=\"d-md-none\">" + str(self.object.schedule.tr_staff2)
         if self.object.schedule.tr_staff3:
-            helpers += "　" + str(self.object.schedule.tr_staff3)
+            helpers += "<br class=\"d-md-none\">" + str(self.object.schedule.tr_staff3)
         if self.object.schedule.tr_staff4:
-            helpers += "　" + str(self.object.schedule.tr_staff4)
+            helpers += "<br class=\"d-md-none\">" + str(self.object.schedule.tr_staff4)
         context['helpers'] = helpers
         
         return context
@@ -555,12 +543,13 @@ class ScheduleEditView(StaffUserRequiredMixin,UpdateView):
         old_data_obj = Schedule.objects.select_related('report').get(id=self.object.pk)
         st = localtime(old_data_obj.start_date)
         ed = localtime(old_data_obj.end_date)
+        sv = old_data_obj.service
 
         report_obj = Report.objects.get(schedule=old_data_obj)
         careuser_comfirmed = report_obj.careuser_comfirmed
 
         #予定時刻が変更された場合
-        if st != self.object.start_date or ed != self.object.end_date:
+        if st != self.object.start_date or ed != self.object.end_date  :
             now  = datetime.datetime.now()
             now  = make_aware(now)
 
@@ -571,16 +560,20 @@ class ScheduleEditView(StaffUserRequiredMixin,UpdateView):
                 new_service_out_date =None
                 #利用者確認済みを解除
                 careuser_comfirmed = False
+
             #現在より過去に移動の場合
             else:
-                #予定時刻に修正する
+                #予定時刻を修正する
                 new_service_in_date  = self.object.start_date
                 new_service_out_date = self.object.end_date
- 
+
+
+
+
             #reportの時刻を修正
-            report_obj = Report.objects.get(schedule=old_data_obj)
-            report_obj.service_in_date = new_service_in_date
-            report_obj.service_out_date = new_service_out_date
+            report_obj.service_in_date    = new_service_in_date
+            report_obj.service_out_date   = new_service_out_date
+
             report_obj.careuser_comfirmed = careuser_comfirmed
             report_obj.save()
 
